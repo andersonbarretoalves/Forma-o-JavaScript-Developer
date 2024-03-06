f064b1eba7bcf6ae356df0cb3531b12dd5afa1fd
Anderson Alves <andersonbarretodev@gmail.com>
2024-03-06 15:15:32 -0300

    Enviando o arquivo numeroPar.js

--- 02- condicionais/numeroPar.js
+++ 02- condicionais/numeroPar.js
@@ -1,5 +1,21 @@
-const numero = 11;
+const numero = 2;
 
-const NumeroPar = (numero % 2) === 0;
+const NumeroPar = (numero % 2) === 0; //== ingnora o tipo e o === deve ser identico.
 
 console.log(NumeroPar);
+
+if (numero === 0)
+{
+    console.log('Numero Invalido!')
+}
+
+else if(NumeroPar)
+{
+    console.log('O numero é Par.');
+}
+
+else
+{
+    console.log('O numero é Impar.');
+}
+
