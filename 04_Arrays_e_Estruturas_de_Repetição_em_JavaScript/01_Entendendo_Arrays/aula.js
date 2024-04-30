5cfc7d076a67ab45ab9e74ac2927b233c7e0575d
Anderson Alves <andersonbarretodev@gmail.com>
2024-04-30 18:09:22 -0300

    Aula sobre Arrays

---
+++ 04_Arrays_e_Estruturas_de_Repetição_em_JavaScript/01_Entendendo_Arrays/aula.js
@@ -0,0 +1,16 @@
+
+
+
+const alunos = ['João','Vitor','Marina'];
+
+alunos.push('Renan');
+
+alunos[4] = 'José';
+
+alunos
+
+console.log(alunos);
+
+console.log(alunos.pop()); // "pop" retira o ultimo e "shift" o primeiro. 
+
+console.log(alunos);
